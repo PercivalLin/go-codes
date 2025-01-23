51c78fc2a63ba44f713d00b08dd7443b01fa5869
PercivalLin <Lin18974@outlook.com>
2025-01-23 09:49:02 +0800

    fixed a bug

--- echo2.go
+++ echo2.go
@@ -1,5 +1,5 @@
-//Echo2 prints its command-line arguments.
-//It provides a new way to write (for)
+// Echo2 prints its command-line arguments.
+// It provides a new way to write (for)
 package main
 
 import (
@@ -13,23 +13,23 @@ func main() {
 	// var s = ""
 	// var s sting = ""
 	// var s, sep string
-	s,sep := "",""
+	s, sep := "", ""
 
-	for _,arg := range os.Args[1:] {
-		s += sep +arg
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
 		sep = ""
 	}
-	/* 
-		"_"is a bland identifier
-        and the for statement has a similar form:
-	   
-	    for initialization; condition; post {
-			// zero of more statement
-	   	}
+	/*
+				"_"is a bland identifier
+		        and the for statement has a similar form:
 
-	    the initialization must be a simple statement(if it exists)
-		the condition is a boolean expression,used to determine if a loop body statement can be executed
-		the post is executed after the end of the loop body
-		*/
-	fmt.PrintLn(s)
+			    for initialization; condition; post {
+					// zero of more statement
+			   	}
+
+			    the initialization must be a simple statement(if it exists)
+				the condition is a boolean expression,used to determine if a loop body statement can be executed
+				the post is executed after the end of the loop body
+	*/
+	fmt.Println(s)
 }
